922756a4c67053d6357a4ca76aa92c1815567012
Pranav Shukla <pranavshukla17@gmail.com>
2018-10-03 09:55:26 +0530

    spring jdbc example with xml configuration

---
+++ spring_Jdbc/com/spring/dao/AccountDao.java
@@ -0,0 +1,11 @@
+package com.spring.dao;
+
+import com.spring.model.Account;
+public interface AccountDao {
+	public void save(Account account);
+	public Account get(int accno);
+	public void update(Account accout);
+	public void remove(int accno);
+	
+	//finder methods
+}
